cf638176f149a9cea337aa343809512cb16f457a
apple <18201497620@163.com>
2020-07-30 14:34:55 +0800

    initCommit

---
+++ app/src/main/java/com/example/gamerockerview/GameRockerView.java
@@ -0,0 +1,23 @@
+package com.example.gamerockerview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+/**
+ * create by libo
+ * create on 2020/7/30
+ * description
+ */
+public class GameRockerView extends View {
+
+    public GameRockerView(Context context) {
+        super(context);
+    }
+
+    public GameRockerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
